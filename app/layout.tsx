b129101d4ee3d2b662410b93d1c627cd3a94097f
Ashutosh <ashutosh2801@gmail.com>
2026-03-04 00:39:33 +0530

    new changes 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Charter Bus Rental Toronto | Group Transportation | GTACoach",
